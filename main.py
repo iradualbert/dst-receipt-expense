7f4f8c6585ebb988fec1993e141cf0c045ff1d1c
Albert Iradukunda <albert.iradukunda@ozu.edu.tr>
2024-12-16 00:20:54 +0300

    started annotations

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-from utils import extract_all_receipts
-invoice_data_categories = [
 
-]
-
-categories = [
-    "Sender Information",
-    "Receiver",
-    "Product information/items",
-    "unit prices/quantiy/total",
-    "Total Amount",
-    "Tax information",
-    "receipt information",
-    "invoice information",
-    "time and date"
-]
-
-extract_all_receipts()
